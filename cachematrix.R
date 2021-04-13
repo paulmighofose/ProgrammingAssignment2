2ea6332157a7642034021ee2811c00a3e3a62695
paulmighofose <59885584+paulmighofose@users.noreply.github.com>
2021-04-13 11:38:17 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-## The function below takes a vector input, calculates the mean, and stores it in a list. 
-makeCacheMatrix <- function(x = matrix()) {
-          m <- NULL
-+         set <- function(y) {
-+                 x <<- y
-+                 m <<- NULL
-+         }
-+         get <- function() x
-+         setmean <- function(mean) m <<- mean
-+         getmean <- function() m
-+         list(set = set, get = get,
-+              setmean = setmean,
-+              getmean = getmean)
+## Explanatory Comment #1: 
+## "The function below takes a vector input, calculates the mean, and stores it in a list." 
 
+cacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
+matrix <- cacheMatrix(1:20)
 
 
-## Write a short comment describing this function
-## This function creates another vector inpu. Thereafter, the function determines whether the vectors mean has been calculated by checking the list created from
-## the function above. If there was no value created it returns a mean. If there was, no mean is calculated and the previously calculated mean is displayed. 
+## Explanatory Comment #2: 
+
+## "This function creates another vector input. Thereafter, the function determines whether the vectors mean has been calculated by checking the list created from
+## the function above. If there was no value created it returns a mean. If there was, no mean is calculated and the previously calculated mean is displayed."
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-          m <- x$getmean()
-+         if(!is.null(m)) {
-+                 message("getting cached data")
-+                 return(m)
-+         }
-+         data <- x$get()
-+         m <- mean(data, ...)
-+         x$setmean(m)
-+         m
-
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
+
+cacheSolve(matrix)
+## Solution = 10.5 
